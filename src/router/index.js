856f753fc62ea15585fdb4791310f42ca6853e68
sld <sld131411@163.com>
2022-08-08 10:25:19 +0800

    login

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import longin from '@/views/login'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/login',
+    name: 'login',
+    component: longin
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <van-nav-bar title="登录" />
+    <van-form @submit="onSubmit">
+      <van-field v-model="username" name="用户名" placeholder="请输入手机号" />
+      <van-field
+        v-model="password"
+        type="password"
+        name="验证码"
+        placeholder="请输入验证码"
+      />
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loginIndex',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit (values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
